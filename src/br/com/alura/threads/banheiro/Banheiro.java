0e59f027c0221e8352692fe6792967f3cefd4ba7
Roberto Nogueira <robertondc@gmail.com>
2016-08-30 00:14:47 -0300

    wait and notify

--- src/br/com/alura/threads/banheiro/Banheiro.java
+++ src/br/com/alura/threads/banheiro/Banheiro.java
@@ -1,7 +1,9 @@
 package br.com.alura.threads.banheiro;
 
-public class Banheiro {	
-	
+public class Banheiro {
+
+	boolean ehSujo = true;
+
 	public void fazNumero1() {
 
 		String nome = Thread.currentThread().getName();
@@ -11,6 +13,11 @@ public class Banheiro {
 		synchronized (this) {
 
 			System.out.println(nome + " entrando no banheiro");
+
+			while (this.ehSujo) {
+				esperaLaFora(nome);
+			}
+
 			System.out.println(nome + " faz coisa rapida");
 
 			try {
@@ -19,6 +26,8 @@ public class Banheiro {
 				e.printStackTrace();
 			}
 
+			this.ehSujo = true;
+			
 			System.out.println(nome + " dando descarga");
 			System.out.println(nome + " lavando a mao");
 			System.out.println(nome + " saindo do banheiro");
@@ -26,14 +35,28 @@ public class Banheiro {
 
 	}
 
+	private void esperaLaFora(String nome) {
+		System.out.println(nome + ", eca, o banheiro está sujo");
+		try {
+			this.wait();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
 	public void fazNumero2() {
-		
+
 		String nome = Thread.currentThread().getName();
 
 		System.out.println(nome + " batendo na porta");
 
 		synchronized (this) {
 			System.out.println(nome + " entrando no banheiro");
+
+			while (ehSujo) {
+				esperaLaFora(nome);
+			}
+
 			System.out.println(nome + " faz coisa demorada");
 
 			try {
@@ -41,12 +64,42 @@ public class Banheiro {
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-
+			
+			this.ehSujo = true;
+			
 			System.out.println(nome + " dando descarga");
 			System.out.println(nome + " lavando a mao");
 			System.out.println(nome + " saindo do banheiro");
 		}
 
+	}
+
+	public void limpa() {
+		String nome = Thread.currentThread().getName();
+		System.out.println(nome + " batendo na porta");
+
+		synchronized (this) {
+			System.out.println(nome + " entrando no banheiro");
+
+			if (!ehSujo) {
+				System.out.println(nome + " , não está sujo e vou sair");
+				return;
+			}
+			
+			System.out.println(nome + " limpando o banheiro");
+		
+			this.ehSujo = false;
+			
+			try {
+				Thread.sleep(13000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			this.notifyAll();
+
+			System.out.println(nome + " saindo do banheiro");
+		}
 	}
 
 }
--- src/br/com/alura/threads/banheiro/Principal.java
+++ src/br/com/alura/threads/banheiro/Principal.java
@@ -6,12 +6,15 @@ public class Principal {
 		
 		Thread convidado1 = new Thread(new TarefaNumero1(banheiro), "Joao");
 		Thread convidado2 =  new Thread(new TarefaNumero2(banheiro), "Pedro");
-		Thread convidado3 =  new Thread(new TarefaNumero2(banheiro), "Maria");
-		Thread convidado4 =  new Thread(new TarefaNumero2(banheiro), "Ana");
+		//Thread convidado3 =  new Thread(new TarefaNumero2(banheiro), "Maria");
+		//Thread convidado4 =  new Thread(new TarefaNumero2(banheiro), "Ana");
+		Thread limpeza =  new Thread(new TarefaLimpeza(banheiro), "Limpeza");
+		limpeza.setDaemon(true); //isso indica que quando as outras threads do processo, tiverem terminado essa pode morrer
 		
 		convidado1.start();
 		convidado2.start();
-		convidado3.start();
-		convidado4.start();
+		limpeza.start();
+		//convidado3.start();
+		//convidado4.start();
 	}
 }
